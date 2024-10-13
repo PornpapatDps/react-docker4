159f3ef104720dc1d4f7d5041dba52509bb682d6
Pornpapat <pornpapat.d@ku.th>
2024-10-13 15:06:34 +0700

    Update MyCard.jsx

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,8 +26,8 @@ export default function MyCard(props) {
     </Typography>
   </CardContent> 
   <CardActions>
-    <Button size="small">XXX</Button>
-    <Button size="small">Learn More</Button>
+    <Button size="small">XXX1</Button>
+    <Button size="small">Learn More</Button> 
   </CardActions>
 </Card>
 
